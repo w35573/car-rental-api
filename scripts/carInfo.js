4d0504407a786923982c448644802849decab5e4
w35573 <prathamesh.redij@spit.ac.in>
2022-09-16 17:36:46 +0530

    add9

--- scripts/carInfo.js
+++ scripts/carInfo.js
@@ -10,48 +10,41 @@ async function getData(city) {
         });
         const page = await browser.newPage();
         const result = [];
-        await page.setViewport({ width: 1200, height: 800 })
-        await page.setRequestInterception(true)
+        await page.setViewport({ width: 1200, height: 800 });
+        await page.setRequestInterception(true);
 
         page.on('request', (request) => {
-            console.log(request.url());
+            // console.log(request.url());
             request.continue()
         })
 
-        // page.on('response', async (response) => {
-        //     try {
-        //         if (response.url() == "https://admin.revv.co.in/api/v1/lt/car/pricing/get") {
-        //             const data = await response.json();
-        //             result.push(data);
-        //             console.log("hello")
-        //         }
-        //     } catch (e) {
-        //         console.log(e);
-        //     }
+        page.on('response', async (response) => {
+            try {
+                if (response.url() == "https://admin.revv.co.in/api/v1/lt/car/pricing/get") {
+                    const data = await response.json();
+                    result.push(data);
+                    console.log("hello")
+                }
+            } catch (e) {
+                console.log(e);
+            }
 
-        //     // console.log(response.url())
-        // })
+            // console.log(response.url())
+        })
 
-        const waitForResponse = (page, url) => {
-            return new Promise(resolve => {
-                page.on("response", function callback(response) {
-                    if (response.url() === url) {
-                        resolve(response);
-                        page.removeListener("response", callback)
-                    }
-                })
-            })
-        };
-
-        const res = await waitForResponse(page, "https://admin.revv.co.in/api/v1/lt/car/pricing/get");
+        // const [res] = await Promise.all([
+        //     page.waitForResponse(res => res.url() === "https://admin.revv.co.in/api/v1/lt/car/pricing/get"),
+        //     page.goto(URL, { waitUntil: "domcontentloaded", timeout: 90000}),
+        // ]);
 
 
+        // console.log(await res.json());
 
-        await page.goto(URL);
+        await page.goto(URL, { waitUntil: 'networkidle0' });
 
         await browser.close();
 
-        return res;
+        return result[0];
 
     } catch (error) {
         throw error;
