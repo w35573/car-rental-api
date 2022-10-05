b8e0d3d9273185e6f6068ba7023ed78f1cc856fb
w35573 <prathamesh.redij@spit.ac.in>
2022-10-05 23:51:39 +0530

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./data/swagger.json');
 const carInfo = require('./scripts/carInfo');
@@ -10,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(cors());
 
 //GET home page
 app.get('/', (req, res) => {
@@ -56,9 +59,9 @@ app.get('/car/:id/:cityId', async (req, res) => {
 //str => short term rental
 //allowed timings -->
 //0:00 4:00 4:30 5:00 5:30 6:00 6:30 7:00 7:30 8:00 8:30 9:00 9:30 10:00 10:30 11:00 11:30 12:00 12:30 13:00 13:30 14:00 14:30 15:00 15:30 16:00 16:30 17:00 17:30 18:00 18:30 19:00 19:30 20:00 20:30 21:00 21:30 22:00 22:30 23:00 23:30
-app.get('/str/:startDate/:endDate/:long/:lat/:location', async function(req, res) {
+app.get('/str/:startDate/:endDate/:long/:lat/:location', async function (req, res) {
     const { startDate, endDate, long, lat, location
-     } = req.params;
+    } = req.params;
     try {
         const data = await carStr.getData(startDate, endDate, long, lat, location);
         res.json(data);
